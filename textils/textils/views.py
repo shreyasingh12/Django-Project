a4e5a157db5aa31da282165b90ad4a46a2d9b6cd
Shreya Singh <sshreya0003@gmail.com>
2020-04-09 23:20:43 +0530

    version8

--- textils/textils/views.py
+++ textils/textils/views.py
@@ -1,16 +1,22 @@
 #i have created this file.
 
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def index(request):
-    return HttpResponse('''<h1>Hello shreya</h1> <a href="https://www.youtube.com/
-    watch?v=zs2Ux1jfDD0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=6" >heyyy</a>''')
+    baabu={'name':'shreya','place':'lockdown'}
+    return render(request,'index.html',baabu)
+   # return HttpResponse('''<h1>Hello shreya</h1> <a href="https://www.youtube.com/
+    #watch?v=zs2Ux1jfDD0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=6" >heyyy</a>''')
 
 
 def about(request):
     return HttpResponse("about shreya")
 
 def removepunc(request):
+    #get text
+    djtext=request.GET.get('text','default')
+    print(djtext)
+    #analyse the text
     return HttpResponse("remove punc")
 
 def capfirst(request):
