07c1bbe7c19dc12abc3109b02b9b1ea10cfa6170
Shreya Singh <sshreya0003@gmail.com>
2020-04-10 15:01:39 +0530

    version 10 with bootstrap

--- textils/textils/views.py
+++ textils/textils/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
     baabu={'name':'shreya','place':'lockdown'}
-    return render(request,'index.html',baabu)
+    return render(request,'index2.html',baabu)
    # return HttpResponse('''<h1>Hello shreya</h1> <a href="https://www.youtube.com/
     #watch?v=zs2Ux1jfDD0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=6" >heyyy</a>''')
 
@@ -12,6 +12,7 @@ def index(request):
 def analyze(request):
     #Get the text
     djtext = request.GET.get('text', 'default')
+    print(djtext)
 
     # Check checkbox values
     removepunc = request.GET.get('removepunc', 'off')
@@ -26,9 +27,38 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
 
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze2.html', params)
+
+    elif(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze2.html', params)
+
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze2.html', params)
+
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze2.html', params)
     else:
         return HttpResponse("Error")
 
